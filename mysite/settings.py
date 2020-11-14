d60a099a548a0006a9ea8272e7f63fab049c2b37
Armengedon <armengolet@gmail.com>
2020-11-14 01:31:05 +0100

    s

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 import django_heroku
 import os
 from pathlib import Path
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
