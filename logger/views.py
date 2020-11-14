6308e3d167468e731fac80180dd657df9ba33b58
Armengedon <armengolet@gmail.com>
2020-11-14 13:01:22 +0100

    new logging

--- logger/views.py
+++ logger/views.py
@@ -5,9 +5,16 @@ from .models import Esp32, Reading
 def index(request):
 	return HttpResponse("Hello, world. You're at the logger index.")
 	
-def registerLogger(request, esp_id, esp_name):
-	logger = Esp32(esp_id=esp_id, esp_name=esp_name)
-	logger.save()
+def registerLogger(request, id, name):
+
+	if not Esp32.objects.filter(esp_id=id).exists():
+		logger = Esp32(esp_id=id, esp_name=name)
+		logger.save()
+	response = ""
+	for logg in Esp32.objects.all():
+		response += str(logg) + "\n"
+	return HttpResponse(response)
+
 	
 def detail(request, esp_id):
 	return HttpResponse("You are looking at esp32 %s." % esp_id)
